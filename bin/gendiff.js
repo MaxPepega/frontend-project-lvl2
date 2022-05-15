cc94422e276f44ee86e24ecef25a96d3b5338161
MaxPepega <budkomaksim16@gmail.com>
2022-05-15 15:21:25 +0500

    add directory test files and utils

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import genDiff from "../src/index.js"
+
 
 program
   .version('1.0.1')
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1> <filepath2>')
-  .option('-f, --format <type>','output format');
-program.parse();
+  .argument('<filepath1 filepath2>')
+  .option('-f, --format <type>','output format')
+  .action((filepath1, filepath2)=>{
+    console.log(genDiff(filepath1, filepath2, program.command))
+  })
+  .parse(process.argv);
