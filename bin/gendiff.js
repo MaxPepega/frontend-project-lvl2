ca406430c418581794fa6c451d741e680a1265bb
MaxPepega <budkomaksim16@gmail.com>
2022-05-15 15:54:16 +0500

    removed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,38 @@
 #!/usr/bin/env node
+import { commander } from 'commander';
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../src/index.js';
 
-import { program } from "commander";
-import genDiff from "../src/index.js"
 
+function readFile(pathString) {
+  const compoosedPath = path.resolve(process.cwd(), pathString);
+  const fileUrl = new URL(`file://${compoosedPath}`);
+  let file;
+  try {
+    file = fs.readFileSync(fileUrl, 'utf8');
 
-program
-  .version('1.0.1')
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1 filepath2>')
-  .option('-f, --format <type>','output format')
-  .action((filepath1, filepath2)=>{
-    console.log(genDiff(filepath1, filepath2, program.command))
-  })
-  .parse(process.argv);
+  } catch(e) {
+    console.log('cant read file')
+    return '{}';
+  }
+  return file;
+}
+
+const program = new commander.Command();
+program.version('0.0.1', '-v, --vers', 'output the current version')
+.description('Usage: gendiff [options]')
+.description('Compares two configuration files and shows a difference.')
+.option('-f, --format [type]', 'output format')
+.action(function (smth, env) {
+
+  const files = env.map(file => {
+    return readFile(file);
+  });
+
+  const [first, second] = files;
+  genDiff(first, second);
+
+});
+
+program.parse(process.argv);
